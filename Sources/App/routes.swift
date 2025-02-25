d4ef8ea713b29cb80aa16be2140a2ec8e8359ce9
Marc Hervera <mhergon@gmail.com>
2025-02-25 18:11:20 +0100

    First update

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,11 +3,11 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async in
-        "It works!"
+        "It works but..."
     }
 
     app.get("hello") { req async -> String in
-        "Hello, world!"
+        "Hello, world Marc!"
     }
 
     try app.register(collection: TodoController())
